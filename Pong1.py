f0ea2a264d182e9d5e1c91c3e143978cd5e5a612
David Morgenstern <DavidtheConfident@gmail.com>
2022-09-20 23:24:26 -0500

    Paddle A Creation

--- Pong1.py
+++ Pong1.py
@@ -4,7 +4,20 @@ wn = turtle.Screen()
 wn.title("PingPongPython by David Morgenstern")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
-wn.tracer(0)
+wn.tracer(0) # Stops the window from updating; speeds up the game
+
+# Paddle A
+paddle_A = turtle.Turtle()
+paddle_A.speed(0)
+paddle_A.shape("square")
+paddle_A.color("white")
+paddle_A.shapesize(stretch_wid=5, stretch_len=1)
+paddle_A.penup()
+paddle_A.goto(-350, 0)
+
+# Paddle B
+
+# Ball
 
 # Main Game Loop
 while True:
