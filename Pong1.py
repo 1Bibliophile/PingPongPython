a858f8bd165576426b4cba18ca97eb690a15bc42
David Morgenstern <DavidtheConfident@gmail.com>
2022-09-20 20:33:33 -0500

    Creates Window

---
+++ Pong1.py
@@ -0,0 +1,11 @@
+import turtle
+
+wn = turtle.Screen()
+wn.title("PingPongPython by David Morgenstern")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# Main Game Loop
+while True:
+    wn.update()
