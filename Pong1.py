d639242527c765d8f56ba65c1d71a39beabb0e73
David Morgenstern <DavidtheConfident@gmail.com>
2022-09-21 21:41:56 -0500

    Creates Scoresheet

--- Pong1.py
+++ Pong1.py
@@ -35,7 +35,15 @@ ball.goto(0, 0)
 ball.dx = 2
 ball.dy = -2
 
-#
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+
+pen.goto(0, 260)
+pen.write("Player A: 0   Player B: 0", align="center", font=("Courier", 24, "normal "))
 
 # Functions
 def paddle_a_up():
