93dec9d97a8318c1e9cae24de98f296e4b95f45e
David Morgenstern <DavidtheConfident@gmail.com>
2022-09-21 20:55:09 -0500

    Adds Paddle Movement Functionality

--- Pong1.py
+++ Pong1.py
@@ -16,9 +16,44 @@ paddle_A.penup()
 paddle_A.goto(-350, 0)
 
 # Paddle B
+paddle_B = turtle.Turtle()
+paddle_B.speed(0)
+paddle_B.shape("square")
+paddle_B.color("white")
+paddle_B.shapesize(stretch_wid=5, stretch_len=1)
+paddle_B.penup()
+paddle_B.goto(350, 0)
 
 # Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.shapesize(stretch_wid=5, stretch_len=1)
+ball.penup()
+ball.goto(0, 0)
+
+# Functions
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+
+# Keyboard Binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+
+
 
 # Main Game Loop
 while True:
     wn.update()
+    turtle.done()
+
