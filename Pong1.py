1e39a2b49065ffbddb5090baf2be1f097b0e986b
David Morgenstern <DavidtheConfident@gmail.com>
2022-10-01 20:50:03 -0500

    Keeps Track of Score

--- Pong1.py
+++ Pong1.py
@@ -6,6 +6,10 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0) # Stops the window from updating; speeds up the game
 
+# Score
+score_a = 0
+score_b = 0
+
 # Paddle A
 paddle_A = turtle.Turtle()
 paddle_A.speed(0)
@@ -97,10 +101,16 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}   Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal "))
 
     if ball.xcor() < -390:
         ball.goto(0 , 0)
         ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}   Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal "))
 
     # Paddle & Ball Collisions
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_B.ycor() + 40 and ball.ycor() > paddle_B.ycor() -40):
@@ -110,3 +120,5 @@ while True:
     if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_A.ycor() + 40 and ball.ycor() -40):
         ball.setx(-340)
         ball.dx *= -1
+
+
